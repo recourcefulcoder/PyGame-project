f713b6d8f2afbe751b457881e3a00f9230ac3c17
kiric <kirichenko.grigorij@inbox.ru>
2021-12-18 18:17:36 +0300

    Improved level-making programm: added ability to set checkpoints and an escape-zone. The way of working of these features was written in the "documentation.txt"

--- developer/LevelMaker.py
+++ developer/LevelMaker.py
@@ -10,10 +10,12 @@ class Board:
         self.board = [["black"] * width for _ in range(height)]
         self.left = 0
         self.top = 0
-        self.chosen_color = 'black'
+        self.chosen_object = 'black'
         self.colors = {"black": (0, 0, 0), "red": (255, 0, 0), "grey": (128, 128, 128),
-                       "brown": (94, 47, 13), "blue": (66, 170, 255), "white": (255, 255, 255)}
+                       "brown": (94, 47, 13), "blue": (66, 170, 255), "white": (255, 255, 255),
+                       "golden": (255, 215, 0)}
         self.cell_size = 30
+        self.font = pygame.font.Font(None, 24)
 
     def set_view(self, left, top, cell_size):
         self.left = left
@@ -26,9 +28,13 @@ class Board:
                 pygame.draw.rect(screen, pygame.Color(255, 255, 255),
                                  ((j * self.cell_size + self.left, i * self.cell_size + self.top),
                                   (self.cell_size, self.cell_size)), width=1)
-                pygame.draw.rect(screen, pygame.Color(*self.colors[self.board[i][j]]),
-                                 ((j * self.cell_size + self.left + 1, i * self.cell_size + self.top + 1),
-                                  (self.cell_size - 2, self.cell_size - 2)))
+                if self.board[i][j] in self.colors.keys():
+                    pygame.draw.rect(screen, pygame.Color(*self.colors[self.board[i][j]]),
+                                     ((j * self.cell_size + self.left + 1, i * self.cell_size + self.top + 1),
+                                      (self.cell_size - 2, self.cell_size - 2)))
+                else:
+                    screen.blit(self.font.render(self.board[i][j], True, (80, 200, 120)),
+                                (j * self.cell_size + self.left + 1, i * self.cell_size + self.top + 1))
 
     def get_cell(self, mouse_pos):
         x = mouse_pos[0] - self.left
@@ -40,7 +46,7 @@ class Board:
         return None
 
     def on_click(self, cell_coords):
-        self.board[cell_coords[0]][cell_coords[1]] = self.chosen_color
+        self.board[cell_coords[0]][cell_coords[1]] = self.chosen_object
 
     def get_click(self, mouse_pos):
         cell = self.get_cell(mouse_pos)
@@ -64,17 +70,36 @@ class ColorBoard(Board):
         self.board[1][0] = "brown"
         self.board[1][1] = "blue"
         self.board[1][2] = "white"
+        self.board[2][0] = "golden"
+        self.board[2][1] = "1"
+        self.current_checkpoint = 1
         self.cell_size = 20
         self.left = win_width - self.cell_size * 3
 
     def on_click(self, cell_coords):
         if cell_coords is not None:
-            return self.board[cell_coords[0]][cell_coords[1]]
+            value = self.board[cell_coords[0]][cell_coords[1]]
+            return value
         return None
 
     def get_click(self, mouse_pos):
         cell = self.get_cell(mouse_pos)
         return self.on_click(cell)
+
+
+class ChangingCheckpoint(Board):
+    def __init__(self):
+        super().__init__(1, 2)
+        self.board[0][0] = "Add"
+        self.board[1][0] = "Del"
+        self.set_view(35 * board.cell_size + 30, color_board.cell_size * 4, 30)
+        self.font = pygame.font.Font(None, 16)
+
+    def get_click(self, mouse_pos):
+        cell = self.get_cell(mouse_pos)
+        if cell is not None:
+            return self.board[cell[0]][cell[1]]
+        return None
 
 
 if __name__ == '__main__':
@@ -95,6 +120,9 @@ if __name__ == '__main__':
     color_board = ColorBoard(width)
     board = Board(35, 30)
     board.cell_size = 20
+
+    changing_checkpoint = ChangingCheckpoint()
+
     if exists(level):
         with open(level, mode='r') as level_file:
             value = level_file.readline()
@@ -123,7 +151,18 @@ if __name__ == '__main__':
                 color = color_board.get_click(event.pos)
                 board.get_click(event.pos)
                 if color is not None:
-                    board.chosen_color = color
+                    board.chosen_object = color
+                value = changing_checkpoint.get_click(event.pos)
+                if value == "Add":
+                    if board.chosen_object == color_board.board[2][1]:
+                        board.chosen_object = str(int(color_board.board[2][1]) + 1)
+                    color_board.board[2][1] = str(int(color_board.board[2][1]) + 1)
+
+                elif value == "Del":
+                    if color_board.board[2][1] > "1":
+                        if board.chosen_object == color_board.board[2][1]:
+                            board.chosen_object = str(int(color_board.board[2][1]) - 1)
+                        color_board.board[2][1] = str(int(color_board.board[2][1]) - 1)
             if event.type == pygame.MOUSEBUTTONUP:
                 mouse_pressed = False
             if event.type == pygame.MOUSEMOTION:
@@ -132,4 +171,5 @@ if __name__ == '__main__':
         screen.fill((0, 0, 0))
         board.render(screen)
         color_board.render(screen)
+        changing_checkpoint.render(screen)
         pygame.display.flip()
